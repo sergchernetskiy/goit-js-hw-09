d7ae40463797b8d1fab825f029e1629b1a97a9af
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-09-27 20:49:04 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let timerId = null;
 
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  refs.btnStop.removeAttribute('disabled');
+  refs.btnStart.disabled = true;
+
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStopClick() {
+  refs.btnStart.removeAttribute('disabled');
+  refs.btnStop.disabled = true;
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
