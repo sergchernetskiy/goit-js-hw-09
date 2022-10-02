e6168a24a6583e9b73108a37b91cd6d4ea323852
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-10-02 16:27:06 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+let delay = Number(formRef.delay.value.trim());
+const delayStep = Number(formRef.step.value.trim());
+const amount = Number(formRef.amount.value.trim());
+//console.log('firstDelay', firstDelay);
+//console.log('delayStep', delayStep);
+//console.log('amount', amount);
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  for (let i = 0; i < amount; i += 1) {
+    const position = i + 1;
+
+    createPromise(position, delay).then(onSuccess).catch(onError);
+
+    delay += delayStep;
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+}
+
+const onSuccess = ({ position, delay }) => {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+    position: 'center-center',
+    useIcon: false,
+    opacity: 0.7,
+  });
+};
+const onError = ({ position, delay }) => {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+    position: 'center-center',
+    useIcon: false,
+    opacity: 0.7,
+  });
+};
